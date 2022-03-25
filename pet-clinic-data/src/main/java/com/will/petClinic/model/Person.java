7cb3e2594c87d7c667352c938dc5b62b83340123
raindogg <wabray17@gmail.com>
2022-03-25 14:23:12 -0500

    Implement BaseEntity

--- pet-clinic-data/src/main/java/com/will/petClinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/will/petClinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.will.petClinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
   private String firstName;
   private String lastName;
 
--- pet-clinic-data/src/main/java/com/will/petClinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/will/petClinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.will.petClinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
   private String name;
 
   public String getName() {
